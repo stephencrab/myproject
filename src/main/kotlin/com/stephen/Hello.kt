bd95e049c495111ff85561971120f978c9ec8a70
stephenhu <i75407263@gmail.com>
2019-01-23 11:31:13 +0800

    test

---
+++ src/main/kotlin/com/stephen/Hello.kt
@@ -0,0 +1,13 @@
+package com.stephen
+
+fun main(args: Array<String>) {
+//    println("Hello kotlin!!!")
+//    Human().hello()
+    val h = Human()
+    h.hello()
+}
+class Human {
+    fun hello() {
+        println("Hello kotlin")
+    }
+}
